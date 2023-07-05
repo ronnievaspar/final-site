3c8e38c6b4618a7fe038fa58d9739f74cf9f1ea9
ronnievaspar <97657477+ronnievaspar@users.noreply.github.com>
2023-07-05 21:28:32 +0530

    Add files via upload
    added final project

---
+++ javascript/script.js
@@ -0,0 +1,7 @@
+function popup() {
+    var blur = document.getElementById("blur");
+    blur.classList.toggle("active");
+    var pop = document.getElementById("pop");
+    pop.classList.toggle("active");
+  }
+  
